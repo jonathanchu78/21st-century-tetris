65299472bb3f5f474e46b5cef68004f7b090076a
Spenser Wong <spenserwong1@gmail.com>
2018-02-17 13:39:22 -0800

    Updated playstate

--- src/playstate.cc
+++ src/playstate.cc
@@ -319,15 +319,6 @@ void PlayState::findBotSpace(int arr[], int& startpos, int& endpos)
 	}
 }
 
-void PlayState::check_all(){
-
-}
-
-void PlayState::check_all_xpos(){
-    //int initialx = tetro->
-    int stop = board->COLS - tetro->width + 1;
-}
-
 void PlayState::release_tetromino() {
     Tetromino* new_tetro = new Tetromino(rand()%7);
     new_tetro->set_position(next_tetro->x, next_tetro->y);
@@ -342,6 +333,8 @@ void PlayState::release_tetromino() {
     int startpos;
     int endpos;
     findBotSpace(board->color[0],startpos,endpos);
+	tetro->set_position(startpos + -tetro->left);
+	
 }
 
 // Update game values.
