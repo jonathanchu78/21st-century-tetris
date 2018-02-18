dbaf4e66b2ccb84829c2f2069ea520819e3d62d0
Jonathan Chu <jonathanchu78@gmail.com>
2018-02-17 20:13:13 -0800

    dog

--- src/playstate.cc
+++ src/playstate.cc
@@ -376,7 +376,7 @@ bool PlayState::checkInBounds(){
             std::cerr << "X IS TOO LOW\n" << std::endl;
             proceed = false;
             return false;
-        }
+        }                                                                     
         if (test_tetro->coords[k][0] + test_tetro->x > 13){
             //test_tetro->x--;
             std::cerr << "X IS TOO HIGH\n" << std::endl;
@@ -412,10 +412,6 @@ void PlayState::check_all(int& x_val, int& num_rot){
                 }
             }
         }
-        for(int i= 0; i < 14; i++){
-            if(color[27][i] != -1);
-        }
-        filled = true;
     }
     if(filled == true && tetro->type == 5){
         x_val = 14; 
@@ -549,7 +545,7 @@ void PlayState::update(GameEngine* game) {
 
         // Assign the time required for tetromino to fall down one block.
         if (tetro->speed_up) {
-            time_till_drop = 0.12f;  // 2x slower than free fall.
+            time_till_drop = 0.04f;  // 2x slower than free fall.
         } else {
             // Drop speed proportional to score.
             time_till_drop = 0.0006f - board->get_score()*acceleration;
